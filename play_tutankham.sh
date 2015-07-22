6e97406d9d7c47a06539bf28656643a0081110c9
Ruslan Aydarkhanov <Aydarkhan@users.noreply.github.com>
2015-07-23 01:00:51 +0200

    submit automatic choice of last net

--- play_tutankham.sh
+++ play_tutankham.sh
@@ -1,5 +1,8 @@
 ROOT=`pwd`
 GAME="tutankham"
-ITER="15000"
 
-./build/dqn_submit -game_name=${GAME} -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/Net/Snapshot-"$GAME"/"$GAME"_iter_"$ITER".caffemodel" -evaluate=true -gui=false
+CUMULATED=$(ls Net/Snapshot-${GAME}/ | grep .cumulated | grep -o "[0-9]*" | sort -n | tail -n 1)
+
+ITER=$CUMULATED
+
+./build/dqn_submit -game_name=${GAME} -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/Net/Snapshot-"$GAME"/"$GAME"_iter_"$ITER".caffemodel.cumulated" -evaluate=true -gui=false
