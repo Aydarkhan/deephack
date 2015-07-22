4dde932142bd4e78cd30b05905671d78cbabfab3
Ruslan Aydarkhanov <Aydarkhan@users.noreply.github.com>
2015-07-22 23:47:24 +0200

    continue, add to last model suffix .cumulated

--- continue_training_seaquest.sh
+++ continue_training_seaquest.sh
@@ -1,6 +1,16 @@
 ROOT="/home/ubuntu/caffe-dqn/dqn-in-the-caffe"
 GAME="seaquest"
-ITER="10000"
+
+NEWEST=$(ls -t Net/Snapshot-${GAME}/ | grep -v .cumulated | head -n 1 | grep -o "[0-9]*")
+CUMULATED=$(ls Net/Snapshot-${GAME}/ | grep .cumulated | grep -o "[0-9]*" | sort -n | tail -n 1)
+
+ITER=$((NEWEST + CUMULATED))
+
+rm Net/Snapshot-"$GAME"/"$GAME"_iter_"${CUMULATED}".caffemodel.cumulated
+mv Net/Snapshot-"$GAME"/"$GAME"_iter_"${NEWEST}".caffemodel  Net/Snapshot-"$GAME"/"$GAME"_iter_"${ITER}".caffemodel.cumulated
+
 
 export OPENBLAS_NUM_THREADS=32
-./build/dqn -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/Net/Snapshot-"$GAME"/"$GAME"_iter_"$ITER".caffemodel"
+./build/dqn -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/Net/Snapshot-"$GAME"/"$GAME"_iter_"$ITER".caffemodel.cumulated"
+
+
