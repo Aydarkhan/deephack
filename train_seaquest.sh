9d43925b94213a209474470ace769340903611e5
Egor Burkov <shrubb@ya.ru>
2015-07-23 12:00:51 +0000

    train_*

--- train_seaquest.sh
+++ train_seaquest.sh
@@ -1,4 +1,4 @@
 ROOT=`pwd`
 GAME="seaquest"
 
-./build/dqn -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" 
+CUDA_VISIBLE_DEVICES=2 ./build/dqn -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -gpu=true -gui=false -evaluate=false
