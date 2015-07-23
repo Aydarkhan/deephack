8391714546595d7a8880748409425f42a373f1b0
Ruslan Aydarkhanov <Aydarkhan@users.noreply.github.com>
2015-07-23 02:28:09 +0200

    submit all in a loop, last model

--- play_seaquest.sh
+++ play_seaquest.sh
@@ -1,9 +1,7 @@
 ROOT=`pwd`
 GAME="seaquest"
 
-CUMULATED=$(ls Net/Snapshot-${GAME}/ | grep .cumulated | grep -o "[0-9]*" | sort -n | tail -n 1)
+NEWEST_GEN=$(ls -t Net/Snapshot-${GAME}/*.caffemod* | head -n 1)
 
-ITER=$CUMULATED
-
-
-./build/dqn_submit -game_name=${GAME} -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/Net/Snapshot-"$GAME"/"$GAME"_iter_"$ITER".caffemodel.cumulated" -evaluate=true -gui=false
+export OPENBLAS_NUM_THREADS=32
+CUDA_VISIBLE_DEVICES=0 ./build/dqn_submit -game_name=${GAME} -solver=$ROOT"/Net/dqn_"$GAME"_solver.prototxt" -rom=$ROOT"/games/"$GAME".bin" -model=$ROOT"/${NEWEST_GEN}" -evaluate=true -gui=false
---
+++ submit_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+    bash submit_gopher.sh
+    bash submit_seaquest.sh
+    bash submit_tutankham.sh
+done
